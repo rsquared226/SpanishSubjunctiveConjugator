f7a4bf12fca1e2cdc08eff32affcf55e0274004d
Rahul Ramkumar <rahulram226@gmail.com>
2016-02-29 17:58:00 -0500

    Create Basic UI
    UI includes:
    - Button to conjugate verb
    - EditText to input infinitive
    - TextView to display verb
    When the button is pressed, the TextView will display what is in
    EditText

--- app/src/main/java/rahulramkumar/spanishsubjunctiveconjugator/MainActivity.java
+++ app/src/main/java/rahulramkumar/spanishsubjunctiveconjugator/MainActivity.java
@@ -2,9 +2,19 @@ package rahulramkumar.spanishsubjunctiveconjugator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+	public void formSubjunctive(View view)
+	{
+		EditText inputInfinitive = (EditText) findViewById(R.id.edittxtInputInfinitive);
+		TextView formedVerb = (TextView) findViewById(R.id.txtviewFormedVerb);
+
+		formedVerb.setText(inputInfinitive.getText().toString()); //Display what is in EditText
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
